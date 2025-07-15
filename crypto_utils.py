461efd5a6572be0e0e7fabc53366c702dd4a2a49
Vicking19 <vikasmallah19@gmail.com>
2025-07-15 09:21:00 +0530

    Initial commit: AES-256 File Encryptor

--- crypto_utils.py
+++ crypto_utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2
@@ -23,9 +24,12 @@ def encrypt_file(file_path, password):
     cipher = AES.new(key, AES.MODE_CBC, iv)
     ciphertext = cipher.encrypt(pad(data))
 
+    ext = os.path.splitext(file_path)[1]
+    metadata = json.dumps({'ext': ext}).encode().ljust(128, b' ')  # 128 bytes
+
     encrypted_path = file_path + ".enc"
     with open(encrypted_path, 'wb') as f:
-        f.write(salt + iv + ciphertext)
+        f.write(salt + iv + metadata + ciphertext)
 
     os.remove(file_path)
 
@@ -35,13 +39,16 @@ def decrypt_file(file_path, password):
 
     salt = file_data[:16]
     iv = file_data[16:32]
-    ciphertext = file_data[32:]
-    key = derive_key(password.encode(), salt)
+    metadata_raw = file_data[32:160].rstrip(b' ')
+    metadata = json.loads(metadata_raw.decode())
+    ext = metadata['ext']
+    ciphertext = file_data[160:]
 
+    key = derive_key(password.encode(), salt)
     cipher = AES.new(key, AES.MODE_CBC, iv)
     data = unpad(cipher.decrypt(ciphertext))
 
-    decrypted_path = file_path.replace('.enc', '.dec')
+    decrypted_path = file_path.replace('.enc', '') + ext
     with open(decrypted_path, 'wb') as f:
         f.write(data)
 
